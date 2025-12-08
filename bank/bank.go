9cda86bf0fc7d3f5298c8e8d9a5efc6157a5a27f
vickky122 <vikrantmani45@gmail.com>
2025-12-08 17:26:52 +0530

    Infinite loops, break and continue

--- bank/bank.go
+++ bank/bank.go
@@ -5,48 +5,55 @@ import "fmt"
 func main() {
 	var accountBalance float64 = 1000
 
-	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit Money")
-	fmt.Println("3. Withdraw Money")
-	fmt.Println("4. Exit")
+	for {
+		fmt.Println("Welcome to Go Bank!")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit Money")
+		fmt.Println("3. Withdraw Money")
+		fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Enter your choice: ")
-	fmt.Scan(&choice)
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scan(&choice)
 
-	if choice == 1 {
-		fmt.Println("Your balance is: ", accountBalance)
-	} else if choice == 2 {
-		var depositBalance float64
-		fmt.Print("Enter the amount to deposit: ")
-		fmt.Scan(&depositBalance)
-		if depositBalance < 0 {
-			fmt.Println("Invalid deposit amount!")
-			return
-		}
-		accountBalance += depositBalance
-		fmt.Println("Your balance is: ", accountBalance)
-	} else if choice == 3 {
-		var withdrawbalance float64
-		fmt.Print("Enter the money to withdraw: ")
-		fmt.Scan(&withdrawbalance)
-		if withdrawbalance < 0 {
-			fmt.Println("Invalid withdraw amount!")
-			return
-		}
-		if withdrawbalance > accountBalance {
-			fmt.Println("Invalid withdraw amount as you cannot withdraw more money than your account balance")
-			return
-		}
+		if choice == 1 {
+			fmt.Println("Your balance is: ", accountBalance)
+		} else if choice == 2 {
+			var depositBalance float64
+			fmt.Print("Enter the amount to deposit: ")
+			fmt.Scan(&depositBalance)
+			if depositBalance < 0 {
+				fmt.Println("Invalid deposit amount!")
+				//return
+				continue
+			}
+			accountBalance += depositBalance
+			fmt.Println("Your balance is: ", accountBalance)
+		} else if choice == 3 {
+			var withdrawbalance float64
+			fmt.Print("Enter the money to withdraw: ")
+			fmt.Scan(&withdrawbalance)
+			if withdrawbalance < 0 {
+				fmt.Println("Invalid withdraw amount!")
+				//return
+				continue
+			}
+			if withdrawbalance > accountBalance {
+				fmt.Println("Invalid withdraw amount as you cannot withdraw more money than your account balance")
+				//return
+				continue
+			}
 
-		accountBalance -= withdrawbalance
-		fmt.Println("Your balance is: ", accountBalance)
-	} else if choice == 4 {
-		fmt.Println("Thank you for using Go Bank!")
-		return
-	} else {
-		fmt.Println("Invalid choice!")
+			accountBalance -= withdrawbalance
+			fmt.Println("Your balance is: ", accountBalance)
+		} else if choice == 4 {
+			fmt.Println("Thank you!")
+			return
+		} else {
+			fmt.Println("Invalid choice!")
+			continue
+		}
 	}
+	fmt.Println("Thank you for using Go Bank!")
 
 }
