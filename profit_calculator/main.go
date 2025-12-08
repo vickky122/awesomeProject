ef7dd84bb4323e80f7ec2cf5c3c65dde64656557
vickky122 <vikrantmani45@gmail.com>
2025-12-08 16:00:42 +0530

    working with functions

--- profit_calculator/main.go
+++ profit_calculator/main.go
@@ -3,22 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxrate float64
+	revenue := inputText("Enter the revenue: ")
+	expenses := inputText("Enter the expenses: ")
+	taxrate := inputText("Enter the tax rate: ")
 
-	fmt.Print("Enter the revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter the expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter the tax rate: ")
-	fmt.Scan(&taxrate)
+	ebt, profit, ratio := calculate(revenue, expenses, taxrate)
 
+	fmt.Printf("EBT: %.2f\n", ebt)
+	fmt.Printf("Profit: %.2f\n", profit)
+	fmt.Printf("Ratio: %.2f\n", ratio)
+}
+
+func inputText(text string) float64 {
+	var userInput float64
+	fmt.Print(text)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculate(revenue, expenses, taxrate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxrate/100)
 	ratio := ebt / profit
-
-	fmt.Println("EBT: ", ebt)
-	fmt.Println("Profit: ", profit)
-	fmt.Println("Ratio: ", ratio)
+	return ebt, profit, ratio
 }
