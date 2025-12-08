c99627bc9ad3ae5f632b41e88af137d7284caf2b
vickky122 <vikrantmani45@gmail.com>
2025-12-08 15:08:46 +0530

    function return values and variable scope

--- Sample/main.go
+++ Sample/main.go
@@ -5,89 +5,47 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	//	fmt.Printf("hi")
-	//
-	//	//solve the error what is coming here
-	//	//compute() is not defined
-	//	//so define it
-	//	func compute() (int, error) {
-	//		return 0, nil
-	//	}
-	//
-	//	if v, err := compute(); err != nil {
-	//		return err
-	//	}
-	//	fmt.Println(v)
-	//
-
-	//if x := 10; x > 5 {
-	//	fmt.Println(x)
-	//}
-	//x := 20
-	//fmt.Println(x)
-	//
-	//
-
-	////for style
-	//for i := 0; i < 10; i++ {
-	//	fmt.Println(i)
-	//}
-	//
-	////for in while style
-	//x := 0
-	//for x < 5 {
-	//	fmt.Println(x)
-	//	x++
-	//}
-	//
-	//s := "नमस्ते"
-	//for idx, r := range s {
-	//	fmt.Println(idx, string(r))
-	//}
-
-	//array and slices
-	//a := [5]int{1, 2, 3, 4, 5}
-	//b := a
-	//b[0] = 10
-	//fmt.Println(a)
-	//fmt.Println(b)
-
-	//var a []int  // nil slice
-	//b := []int{} // empty slice, not nil
-	//fmt.Println(a)
-	//fmt.Println(b)
-
-	//pointers
-	//x := 10
-	//p := &x
-	//fmt.Println(*p)
 
 	Hello()
 
-	const inflationRate = 2.5
 	var price float64
 	var rate float64
 	var year float64
 
-	fmt.Printf("Enter the price: ")
+	//fmt.Printf("Enter the price: ")
+	outputText("Enter the price:")
 	fmt.Scan(&price)
 
-	fmt.Printf("Enter the rate: ")
+	//fmt.Printf("Enter the rate: ")
+	outputText("Enter the rate: ")
 	fmt.Scan(&rate)
 
-	fmt.Printf("Enter the year: ")
+	//fmt.Printf("Enter the year: ")
+	outputText("Enter the year: ")
 	fmt.Scan(&year)
 
-	amount := price * math.Pow(1+rate/100, year)
-	futureRealValue := amount / math.Pow(1+inflationRate/100, year)
+	famount, fRealValue := calculateFutureValues(price, rate, year)
+
+	//amount := price * math.Pow(1+rate/100, year)
+	//futureRealValue := amount / math.Pow(1+inflationRate/100, year)
 	//fmt.Println(amount, futureRealValue)
 	//fmt.Println(futureRealValue)
 
-	formattedFV := fmt.Sprintf("Future Value: %.2f\n", amount)
-	formattedRFV := fmt.Sprintf("Future value(adjusted for inflation: %.2f\n", futureRealValue)
+	formattedFV := fmt.Sprintf("Future Value: %.2f\n", famount)
+	formattedRFV := fmt.Sprintf("Future value(adjusted for inflation: %.2f\n", fRealValue)
 	//fmt.Printf("Future Value: %T\n%0.2f\nFuture value(adjusted for inflation): %0.2f\n", amount, amount, futureRealValue)
 	//%T for amount, then two decimal places for amount, then two decimal places for futureRealValue
 
 	fmt.Print(formattedFV, formattedRFV)
+}
+func outputText(text string) {
+	fmt.Print(text)
+}
+func calculateFutureValues(price, rate, year float64) (float64, float64) {
+	famount := price * math.Pow(1+rate/100, year)
+	fRealValue := famount / math.Pow(1+inflationRate/100, year)
+	return famount, fRealValue
 }
