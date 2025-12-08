6d39c32c6d331c61bda347474b24df3dde06f119
vickky122 <vikrantmani45@gmail.com>
2025-12-08 13:05:24 +0530

    profit calculator app

--- profit_calculator/main.go
+++ profit_calculator/main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("inside profit")
+	var revenue float64
+	var expenses float64
+	var taxrate float64
+
+	fmt.Print("Enter the revenue: ")
+	fmt.Scan(&revenue)
+	fmt.Print("Enter the expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Print("Enter the tax rate: ")
+	fmt.Scan(&taxrate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxrate/100)
+	ratio := ebt / profit
+
+	fmt.Println("EBT: ", ebt)
+	fmt.Println("Profit: ", profit)
+	fmt.Println("Ratio: ", ratio)
 }
