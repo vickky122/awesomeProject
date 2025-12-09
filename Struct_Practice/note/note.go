35686cbdb61ba3d6a1240d8b4ccb14a22852a74f
vickky122 <vikrantmani45@gmail.com>
2025-12-09 15:37:54 +0530

    added the json extention to the file and also the content should be visible in the json file

--- Struct_Practice/note/note.go
+++ Struct_Practice/note/note.go
@@ -10,18 +10,18 @@ import (
 )
 
 type Note struct {
-	title     string
-	content   string
-	createdAt time.Time
+	Title     string
+	Content   string
+	CreatedAt time.Time
 }
 
 func (note Note) Display() {
-	fmt.Printf("Your note title %v has the following content:\n\n%v\n", note.title, note.content)
+	fmt.Printf("Your note title %v has the following content:\n\n%v\n", note.Title, note.Content)
 }
 
 func (note Note) Save() error {
-	fileName := strings.ReplaceAll(note.title, " ", "_")
-	fileName = strings.ToLower(fileName)
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
 
 	json, err := json.Marshal(note)
 	if err != nil {
@@ -35,8 +35,8 @@ func New(title, content string) (Note, error) {
 		return Note{}, errors.New("Invalid input")
 	}
 	return Note{
-		title:     title,
-		content:   content,
-		createdAt: time.Now(),
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
 	}, nil
 }
