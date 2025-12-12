456ec86fe5b6e4802ca008f8bf2372a2c94b478b
vickky122 <vikrantmani45@gmail.com>
2025-12-12 12:20:14 +0530

    panic fix

--- channels/basics/main.go
+++ channels/basics/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -9,37 +10,39 @@ func main() {
 	defer fmt.Println("Main goroutine exited.")
 	defer fmt.Println("Main function completed")
 	defer fmt.Println("checking defer")
+
 	message := make(chan string)
 	message2 := make(chan int)
 	message3 := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
 	go func() {
+		defer wg.Done()
 		time.Sleep(time.Second * 2)
+
 		message <- "Hello from goroutine!"
 		message <- "one more time"
 		message2 <- 1
 		message3 <- 3
+
+		close(message)
+		close(message2)
+		close(message3)
 	}()
+
 	msg := <-message
 	fmt.Println(msg)
 
 	msg = <-message
 	fmt.Println(msg)
 
-	time.Sleep(time.Second * 2)
-	//close(message2)
-
 	msg2 := <-message2
 	fmt.Println(msg2)
 
-	close(message2)
-	//msg := <-message
-
-	fmt.Println(msg)
-	close(message)
-	//close(message)
-	//close(message2)
-	close(message3)
-
 	msg3 := <-message3
 	fmt.Println(msg3)
+
+	wg.Wait()
 }
